8a26655c224eb463f315180f9c368fe89731f1ec
ingchivas <51966857+cmedit@users.noreply.github.com>
2022-12-15 13:52:02 -0600

    amogus

---
+++ contracts/nftickets/src/io.rs
@@ -0,0 +1,85 @@
+use gear_lib::multitoken::io::*;
+use gstd::{prelude::*, ActorId};
+
+#[derive(Debug, Encode, Decode, TypeInfo)]
+#[codec(crate = gstd::codec)]
+#[scale_info(crate = gstd::scale_info)]
+
+pub enum EventActions {
+    Create{
+        event_id: u128,
+        name: String,
+        description: String,
+        available_tickets: u128,
+        date: u128,
+        location: String,
+    },
+    Hold,
+    Purchase {
+        ammount: u128,
+        metadata: Vec<Option<TokenMetadata>>
+    },
+}
+
+#[derive(Debug, Encode, Decode, TypeInfo)]
+#[codec(crate = gstd::codec)]
+#[scale_info(crate = gstd::scale_info)]
+
+pub enum EventData {
+    Create{
+        creator: ActorId,
+        event_id: u128,
+        tickets: u128,
+        date: u128,
+        location: String,
+    },
+    Hold{
+        event_id: u128,
+    },
+    Purchase {
+        event_id: u128,
+        ammount: u128,
+    },
+}
+
+#[derive(Debug, Encode, Decode, TypeInfo)]
+#[codec(crate = gstd::codec)]
+#[scale_info(crate = gstd::scale_info)]
+
+pub enum EventStateQ{
+    CurrentEvent,
+    Buyers,
+    UserTickets {
+        user: ActorId,
+    },
+}
+
+#[derive(Debug, Encode, Decode, TypeInfo)]
+#[codec(crate = gstd::codec)]
+#[scale_info(crate = gstd::scale_info)]
+pub enum EventStateR{
+    CurrentEvent {
+        name: String,
+        description: String,
+        date: u128,
+        location: String,
+        event_id: u128,
+        tickets_available: u128,
+        tickets_sold: u128,
+        date: u128,
+    },
+    Buyers {
+        buyers: BTreeSet<ActorId>,
+    },
+    UserTickets {
+        tickets: Vec<u128>,
+    },
+}
+
+#[derive(Debug, Encode, Decode, TypeInfo)]
+#[codec(crate = gstd::codec)]
+#[scale_info(crate = gstd::scale_info)]
+pub struct InitData {
+    pub owner: ActorId,
+    pub contract_id: ActorId,
+}
--- contracts/nftickets/src/lib.rs
+++ contracts/nftickets/src/lib.rs
@@ -1,14 +1,75 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#![no_std]
+
+pub mod io;
+
+use gear_lib::multitoken::io::*;
+use gstd::{msg, prelude::*, ActorId};
+use multitoken_io::*;
+
+use crate::io::*;
+
+const ZERO_ID: ActorId = ActorId::zero();
+const NFT_COUNT: u128 = 100;
+
+#[derive(Debug, Encode, Decode, TypeInfo)]
+pub struct Event {
+    // CreatorData
+    pub owner_id: ActorId,
+    pub contract: ActorId,
+    pub creator : ActorId,
+
+    // EventData
+    pub name: String,
+    pub description: String,
+    pub event_id: u128,
+
+
+    // TicketData
+    pub available_tickets: u128,
+    pub date: u128,
+    pub location: String,
+    pub tickets: u128,
+    pub ticket_id: u128,
+    
+    // TicketBuyerData
+    pub buyers_data : BTreeSet<ActorId>,
+
+    // Misc
+    pub ct_id : u128,
+    pub active: bool,
+    pub metadata: BTreeMap<ActorId, BTreeMap<u128, Option<TokenMetadata>>>,
+
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+static mut CONTRACT: Option<Concert> = None;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[no_mangle]
+pub unsafe extern "C" fn init() {
+    let cfg : InitData = msg::load().expect("Failed to load init data");
+    let event = Event {
+        owner_id: cfg.owner_id,
+        contract: cfg.contract,
+        ..Default::default()
+    };
+    CONTRACT = Some(Concert::new(event));
+}
+
+#[gstd::async_main]
+async unsafe fn main()
+{
+    let actions : EventActions = msg::load().expect("Failed to load actions");
+    let event : &mut Event = unsafe {
+        CONTRACT.get_or_insert(Default::default())
+    };
+    match actions {
+        EventActions::Create{event_id, name, description, available_tickets, date, location} => {
+            event.create(event_id, name, description, available_tickets, date, location).await;
+        },
+        EventActions::Hold => {
+            event.hold().await;
+        },
+        EventActions::Purchase{ammount, metadata} => {
+            event.purchase(ammount, metadata).await;
+        },
     }
 }
